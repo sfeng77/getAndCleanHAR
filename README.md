06f8355a6ae5fbee5efd0975d2fc0e1fca06b0cb
DrMushroom <sfeng77@gmail.com>
2017-03-10 22:00:31 -0500

    Update to average the variables for each subject.

--- README.md
+++ README.md
@@ -10,20 +10,29 @@ https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Datas
 # A list of files in this repo
 * `README.md`   
 This file.
+
 * `CodeBook.md`
 The codebook describes the variables, the data, and the steps performed to clean up the data.
+
 * `run_analysis.R`  
 The R script to perform the steps described in the codebook.
-In this script, a function called `getAndClean` is defined. This function load the data and produce the tidy dataset.
-* `tidyDataSet.txt`
+In this script, three functions are defined as following:
+  * `getAndLabel` Read the dataset and labels, combine training set and test set data,
+  select the `mean` and `std` variables, and apply the labels.
+  * `average_data`  Calculate the average of each variable, for each subject
+  and each activity, and store the data in a new dataframe.
+
+  * `run_analysis` Call the previous two functions to generate the tidy dataset, and write to the output file.
+
+* `tidyDataset.txt`
 The txt file contains the tidy dataset.
-* `measurement_names.txt`
-The txt file that contains the name for all measurements included in the dataset.
+
 
 # How to run
 * Download and unzip the original dataset.
 * In R console, run the following command:
 ```R
 source("run_analysis.R")
+run_analysis()
 ```
-* Inspect and use the `df` dataframe as needed.
+* Inspect output file as needed.
--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,29 @@
-getAndClean <- function(){
-    traindata <- read.table("UCI HAR Dataset/train/X_train.txt")
-    testdata <- read.table("UCI HAR Dataset/test/X_test.txt")
-    activity.name <- read.table("UCI HAR Dataset/activity_labels.txt")
+getAndLabel <- function(){
+    df <- rbind( read.table("UCI HAR Dataset/train/X_train.txt"), read.table("UCI HAR Dataset/test/X_test.txt"))
+
     feature.name <-  read.table("UCI HAR Dataset/features.txt")
-    trainlabel <- read.table("UCI HAR Dataset/train/y_train.txt")
-    testlabel <- read.table("UCI HAR Dataset/test/y_test.txt")
-    
-    df <- rbind(traindata, testdata)
     colnames(df) <- feature.name$V2
-    select <- (grepl("mean", feature.name$V2) | grepl("std", feature.name$V2)) & !grepl("meanFreq", feature.name$V2)
+
+    select <- (grepl("mean", feature.name$V2) | grepl("std", feature.name$V2)) & !grepl("meanFreq", feature.name$V2) 
     df1 <- df[,select]
+
+    activity.name <- read.table("UCI HAR Dataset/activity_labels.txt")    
+    activity = rbind(read.table("UCI HAR Dataset/train/y_train.txt"),  read.table("UCI HAR Dataset/test/y_test.txt"))
+    subjects = rbind(read.table("UCI HAR Dataset/train/subject_train.txt"), read.table("UCI HAR Dataset/test/subject_test.txt"))
     
-    labels = rbind(trainlabel, testlabel)
-    colnames(labels) <- c("Activity.label")
-    df2 <- cbind(df1, labels)
-    df2$Activity.name <- activity.name$V2[df2$Activity.label]
-    
-    df3 <- split(df2, df2$Activity.name)
-    deselect = 
-    df4 <- sapply(df3, function(x) colMeans(x[-c(ncol(x), ncol(x)-1)]))
-    ncol(data)
-    df4
+    df1$Activity.name <- activity.name$V2[unlist(activity)]
+    df1$Subject <- as.factor(subjects$V1)
+    df1
 }
 
-df <- getAndClean()
+average_data <- function(df){
+    dat <- subset(df, select = -c(Subject, Activity.name))
+    df2 <- aggregate(dat, by = list(Subject = df$Subject, Activity = df$Activity.name), FUN = "mean")
+    df2
+}
+
+run_analysis <- function(){
+    df <- getAndLabel()
+    df2 <- average_data(df)
+    write.table(df2,"tidyDataset.txt", row.names = FALSE)
+}
